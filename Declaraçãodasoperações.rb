37329ab2c9ce630ddcf3fe57f637e6149b96ab80
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-02-01 11:29:23 -0300

    1

---
+++ Declaraçãodasoperações.rb
@@ -0,0 +1,23 @@
+#Realiza um código que cálcula 8+6 e um outro cálculo do resultado anterior  que possa dividir
+#por 2 no começo conter uma mensagem que imprima dessa maneira "Começando a Programar em Ruby.
+
+#Declaração das váriaveis já realizando um cálculo.
+calculo1=8+6;
+calculo2=calculo1/2;
+
+#Imprimir a mensagem para classificar conforme foi definida anteriormente.
+puts"Começando a Programar em Ruby\n";
+#Imprimir a mensagem do resultado.
+puts"Realizando uma operação de exemplo\n";
+puts"-----------------------------------------------------\n";
+puts "Imprimir o resultado da operação simples=" 
+puts calculo1;
+puts"-----------------------------------------------------\n";
+puts "Imprimir o resultado da  segunda operação simples=" 
+puts calculo2;
+puts"-----------------------------------------------------\n";
+#Imprimir a mensagem final.
+puts"Finalização da execução!\n"; 
+
+#end
+

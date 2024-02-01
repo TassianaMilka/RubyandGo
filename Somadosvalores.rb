f4c4f6dd66569befe4e47b0a00d5777b4ebe0315
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-01-31 22:38:08 -0300

    1

---
+++ Somadosvalores.rb
@@ -0,0 +1,23 @@
+
+#Realiza um código destinado a soma de dois valores que são 23.00+17.50 e também conter 
+#uma validação de dado se o resultado é menor a 14 ou maior a 21.
+
+#Declaração das váriaveis e a realização do cálculo.
+valor=23.00+17.50;
+
+#Imprimir o resultado total do valor.
+puts"O valor total="
+puts valor
+#Validação de dado se o valor é menor.
+if(valor<14)
+
+#Imprimir a mensagem para classificar se é menor.
+puts"\nO valor é menor que R$14.00 Reais" 
+
+#Validação de dado se o valor é maior.
+elsif(valor>21)
+
+#Imprimir a mensagem para classificar se é maior.
+puts"\nO valor é maior que R$21.00 Reais"
+    
+end

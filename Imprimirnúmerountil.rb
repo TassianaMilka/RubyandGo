fa27658ae96eaee4419fffb6c88cd51e526a5fcc
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-02-01 12:11:49 -0300

    3

---
+++ Imprimirnúmerountil.rb
@@ -0,0 +1,14 @@
+#Faça um código que contém until para imprimir 50 números,uma soma de inclementação e imprimir o resultado,
+#porém não vai aparecer na compilação,ou seja,é nulo a sua resolução.
+
+#Declaração das variáveis.
+numero=50;
+#Cálculo da soma.
+soma=+50;
+#Repetição de laço.
+until numero == 50 do
+    
+#Imprimir o resultado da soma.
+print"A soma dos 50 números"
+#print soma
+end

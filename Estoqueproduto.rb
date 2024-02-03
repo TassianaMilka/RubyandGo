7b4bdc0841c245dc178414236f4eac831c5139d5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-02-02 21:50:27 -0300

    7

---
+++ Estoqueproduto.rb
@@ -0,0 +1,26 @@
+#Faça utilizando string uma declaração das váriaveis contenha no começo "Temos os melhores
+#produto para sua economia e uma validação de dado se é igual com uma classificação
+#"Não contém no estoque" ou se diferente igual "Contém no estoque".
+
+#Declaração das váriaveis.
+nomeproduto=1;
+comprarproduto="Temos os melhores produtos para sua economia.";
+
+#Imprimir a mensagem.
+printf comprarproduto;
+puts"\n";
+#Imprimir  para classificar na continuação.
+puts"\nLembrando se contém ou ainda vai chegar no estoque!\n";
+puts"\n";
+#Validação de dado se é igual ou diferente igual.
+ if(nomeproduto==1)
+#Imprimir a mensagem destinada para classificar se é igual.
+puts"Não contém no estoque.\n"
+elsif(nomeproduto==2)
+     
+#Imprimir a mensagem destinada para classificar se é  diferente igual.
+puts "Contém no estoque Madeira."
+
+ end
+
+      

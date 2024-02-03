74c93cf8cd686bb8438df24481e384ba8e1c2727
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-02-02 21:48:40 -0300

    4

---
+++ Dividirnúmero.rb
@@ -0,0 +1,17 @@
+#Faça um código que imprima 12,3,1 dentro de uma array e depois dividir
+#o 12 por 3.
+
+
+#Declaração da variável de array.
+numeros = [12, 3, 1];
+#Imprimir as array  conforme a  ordem declarada.
+puts numeros[0] 
+puts numeros[1]
+puts numeros[2] 
+
+#Dividir os números da array.
+divi=numeros[0]/numeros[1];
+#Imprimir o resultado da divisão.
+puts"--------------DIVISÃO---------------"
+puts "O resultado da divisão"
+puts divi

c836db3aef7803be3cd2ab3d1f00304eb4780b1b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-01 22:19:46 -0300

    Update and rename NúmerosBinários to NúmerosBinários.go

---
+++ Go/NúmerosBinários/NúmerosBinários.go
@@ -0,0 +1,21 @@
+
+//Faça um código que imprima os 0 e 1 dos números binários.
+
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {  //Função principal do programa 
+
+     //Declaração das variáveis
+    
+    var numeroP= 0;
+    var numeroS = 1;
+
+    //Imprimir o resultado
+    fmt.Println("----Imprimir os Números Computacional----");
+    fmt.Println(numeroP);
+    fmt.Println(numeroS);
+    
+}

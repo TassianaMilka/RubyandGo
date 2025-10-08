cf68132cf52c2ad20e81cbaafed4b25b4e452914
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-08 14:00:49 -0300

    Create Númeroscálculos.go

---
+++ Go/Númeroscálculos/Númeroscálculos.go
@@ -0,0 +1,21 @@
+//Faça um código que realiza a soma de 120 a ele mesmo e depois o resultado seja novamento os mesmo números.
+
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {  //Função principal do programa 
+
+    //Declaração das variáveis e cálculos
+    
+    var soma= 120+120;
+    var sub=soma-120;
+ 
+
+    //Imprimir o resultado
+    fmt.Println("----Imprimir o Resultado----");
+    fmt.Println(sub);
+  
+    
+}

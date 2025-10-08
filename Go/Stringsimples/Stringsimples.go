51601503e8497266158662872cc08e2cdfc0de81
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-08 14:13:01 -0300

    Create Stringsimples.go

---
+++ Go/Stringsimples/Stringsimples.go
@@ -0,0 +1,20 @@
+//Realiza uma string descrito com frase para identificar.
+
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {  //Função principal do programa 
+
+     //Declaração das variáveis e cálculos
+    
+     frase:="Exemplo de frase em string! ";
+  
+
+    //Imprimir a mensagem do exemplo em string 
+    
+    fmt.Println(frase);
+  
+    
+}

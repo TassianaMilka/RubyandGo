2817aa9c963e69bb3f8af6b310b609ee7090df5f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-13 22:21:31 -0300

    Create Produto_valor.rb

---
+++ Ruby/object oriented/Classprdoutovalor/Produto_valor.rb
@@ -0,0 +1,23 @@
+# Nome da classe
+class Produto_valor
+  # Método construtor
+  def initialize(nome_produto, preco,desconto)
+    @nome_produto = nome_produto;
+    @preco=preco;
+    @desconto=desconto;
+    @calculo=preco-desconto;
+    
+  end
+
+  # Método para exibir informações
+  def produto
+    puts "#{@nome_produto} do valor de R$ #{@preco} com desconto #{@desconto} e preço final R$ #{@calculo}."
+  end
+end
+
+# Dados
+dados = Produto_valor.new("Blusa de Frio", 100,70)
+
+
+# Chamando o método 
+dados.produto

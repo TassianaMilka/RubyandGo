a0165d96ecdab9b0cb4315aad8380d9896e9c92b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-20 21:20:21 -0300

    Create Valorescalculo.go

---
+++ Go/Basic/Valorescalculo/Valorescalculo.go
@@ -0,0 +1,17 @@
+
+//Faça um código destinado para saber o cáclulo de valores R$900,00 e R$900,00 o seu resultado final.
+
+package main //Ponto de entrada
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {
+
+   //Declaração de variavel e realização do cálculo
+    
+   var calculo float32=900+990;
+
+    //Imprimir o resultado
+    
+    fmt.Println("O valor final R$",calculo);
+}

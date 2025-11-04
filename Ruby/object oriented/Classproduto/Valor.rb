657b829fd8fefea8114ce14225ec396c2d24a735
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-04 01:55:46 -0300

    Create Valor.rb

---
+++ Ruby/object oriented/Classproduto/Valor.rb
@@ -0,0 +1,43 @@
+#Realiza um código de classe representada pela função def que dentro em cada uma descrever a mensagem de 
+#exibição,o resultado cálculo de soma com subtração e estrutura de loop com condição para exibir durante 
+#compilação.
+
+#Nome da classe
+class Valor
+  def produto
+    "O resultado do valor"
+  end
+end
+
+#Nome da classe
+class Produtovalor
+  def produto
+      #Realizar o cálculo
+      resul=800+200-80;
+  end
+end
+
+#Nome da classe
+class Produtoval
+  def produto
+        result=100;
+        for i in 1..1
+            if (result>=100  ||  result<=100)
+                return "Está no nível de valor";
+           elif
+                return "Compila o código que está com erro na exibição";
+             end
+          end
+  end
+end
+
+#Função 
+def resultado(objeto)
+  puts objeto.produto
+    
+end
+
+resultado(Valor.new)
+resultado(Produtovalor.new)
+resultado(Produtoval.new)
+      

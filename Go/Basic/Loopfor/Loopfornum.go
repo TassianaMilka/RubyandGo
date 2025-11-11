3e6a0410b9010d5ca2eed3a95c1360883e03322d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-10 21:06:39 -0300

    Create Loopfornum.go

---
+++ Go/Basic/Loopfor/Loopfornum.go
@@ -0,0 +1,21 @@
+//Imprima um código que contenha em ordem de 0 até 8 os números.
+
+
+package main //Ponto de entrada
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {
+
+   
+
+    //Laço de loop
+    for i:=0;i<9;i++{
+
+
+    //Imprimir os números
+    fmt.Println(i);     
+        
+   }
+
+}

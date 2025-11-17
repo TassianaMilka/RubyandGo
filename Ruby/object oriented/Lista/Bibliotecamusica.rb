85a407971fbdc2ef2e6cecaad9db7dd219cf3445
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-17 01:31:55 -0300

    Create Bibliotecamusica.rb

---
+++ Ruby/object oriented/Lista/Bibliotecamusica.rb
@@ -0,0 +1,28 @@
+# 
+
+class Bibliotecamusica 
+  def initialize
+    @musica = []   
+  end
+
+  # Getter
+  def musicas
+    @musica
+  end
+
+  # Setter (adiciona uma musica)
+  def adicionar_musica(musica)
+    @musica << musica
+  end
+
+  # Método extra (remover)
+  def remover_musica(musica)
+    @livros.delete(musica)
+  end
+end
+
+m = Bibliotecamusica.new
+m.adicionar_musica("Small girl feat. Lee Youngji (D.O.) 2024");
+
+
+puts m.musicas.inspect

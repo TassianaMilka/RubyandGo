90925f42674aead8b46d3dd3160ac81be47ccea5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-01-23 14:01:54 -0300

    Create Ifandelse.go

---
+++ Go/Basic/Ifandelse/Ifandelse.go
@@ -0,0 +1,26 @@
+//Elabora a string e validação de dado para imprimir o nome da cor de DarkSalmon.
+
+
+package main //Ponto de entrada
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() {
+
+     //Declaração da variável string
+
+    string := "DarkSalmon";
+
+    //Estrutura de validação do dado
+    if(string=="DarkSalmon"){
+
+    //Imprimir 
+    fmt.Println("DarkSalmon")
+        
+} else {
+        
+    //Imprimir
+    fmt.Println("Color!")
+  }
+}
+    
